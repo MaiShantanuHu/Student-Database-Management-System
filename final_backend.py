2f6af21876d41542bafdc576eed16772dda5f3b3
shantanu <shantanujc@gmail.com>
2020-08-06 21:53:51 +0530

    Add backend code

---
+++ final_backend.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+
+
+
+
+def addStdRec(StdID, Name, Branch, Gender, DoB, Mobile,Email):
+    con = sqlite3.connect("studentrecord.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO studentmanagement VALUES (NULL,?,?,?,?,?,?,?) ",(StdID, Name, Branch, Gender, DoB, Mobile, Email))
+    con.commit()
+    con.close()
+
+
+def deleteRec(id):
+    con = sqlite3.connect("studentrecord.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM studentmanagement WHERE id=?", (id,))
+    con.commit()
+    con.close()
+
+
+
+
+
